13732376a3de41d2edb65d4b18a0e8674e7e0d01
JackTJC <tianjincai@hotmail.com>
2020-10-21 12:02:23 +0800

    "unfinished sr"

--- lab2/StopWait/include/GBNRdtReceiver.h
+++ lab2/StopWait/include/GBNRdtReceiver.h
@@ -10,29 +10,35 @@ class GBNRdtReceiver: public RdtReceiver
 {
 private:
     int expectedSeqNum;
+    Packet sndPkt;
 public:
     GBNRdtReceiver();
     void receive(const Packet &packet);
 };
 GBNRdtReceiver::GBNRdtReceiver():expectedSeqNum(1) {
-
+    this->sndPkt.seqnum=0;
+    this->sndPkt.acknum=0;
+    this->sndPkt.checksum=0;
+    this->sndPkt.checksum=pUtils->calculateCheckSum(this->sndPkt);
 }
 void GBNRdtReceiver::receive(const Packet &packet) {
     //检验校验和
     int checkSum=pUtils->calculateCheckSum(packet);
-    if(checkSum==packet.checksum&&packet.seqnum>0)
+    if(checkSum==packet.checksum&&packet.seqnum==this->expectedSeqNum)
     {
         pUtils->printPacket("接受方接收正确的报文",packet);
         Message msg;
         memcpy(msg.data, packet.payload, sizeof(packet.payload));
         pns->delivertoAppLayer(RECEIVER, msg);
-        Packet ackPkt;
-        ackPkt.acknum=this->expectedSeqNum;
-        ackPkt.checksum=0;
-        ackPkt.checksum=pUtils->calculateCheckSum(ackPkt);
-        pUtils->printPacket("接受方发送确认报文",ackPkt);
-        pns->sendToNetworkLayer(SENDER,ackPkt);
+        this->sndPkt.acknum=this->expectedSeqNum;
+        this->sndPkt.checksum=0;
+        this->sndPkt.checksum=pUtils->calculateCheckSum(this->sndPkt);
+        pUtils->printPacket("接受方发送确认报文",this->sndPkt);
+        pns->sendToNetworkLayer(SENDER,this->sndPkt);
         this->expectedSeqNum++;
     }
+    else
+        pns->sendToNetworkLayer(SENDER,this->sndPkt);
+
 }
 #endif //STOPWAIT_GBNRDTRECEIVER_H
